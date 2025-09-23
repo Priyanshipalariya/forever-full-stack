97e83e057f67bfd980310aa25c13171ad86fbe79
Priyanshi Palariya <priyapalariya26@gmail.com>
2025-09-24 01:21:21 +0530

    modified mongodb file for production

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,19 +1,10 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    try {
-        const uri = process.env.MONGODB_URI;
-        if (!uri) {
-            throw new Error('MONGODB_URI is not set');
-        }
-        mongoose.connection.on('connected', () => {
-            console.log('DB connected');
-        })
-        await mongoose.connect(uri)
-    } catch (err) {
-        console.error('MongoDB connection error:', err?.message || err);
-        throw err;
-    }
+    mongoose.connection.on('connected', () => {
+        console.log('DB connected');
+    })
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
 }
 
 export default connectDB;
